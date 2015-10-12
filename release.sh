21d4e38109d15f89507b4282280e0ad439befd8a
Alan deLevie <adelevie@gmail.com>
2015-10-12 14:45:32 -0400

    Added hacky release script

---
+++ release.sh
@@ -0,0 +1,6 @@
+cp mruby/build/i386-apple-darwin14/bin/pdfq releases/pdfq-i386-apple-darwin14
+cp mruby/build/i686-pc-linux-gnu/bin/pdfq releases/pdfq-i686-pc-linux-gnu
+cp mruby/build/i686-w64-mingw32/bin/pdfq.exe releases/pdfq-i686-w64-mingw32.exe
+cp mruby/build/x86_64-apple-darwin14/bin/pdfq releases/pdfq-x86_64-apple-darwin14
+cp mruby/build/x86_64-pc-linux-gnu/bin/pdfq releases/pdfq-x86_64-pc-linux-gnu
+cp mruby/build/x86_64-w64-mingw32/bin/pdfq.exe releases/pdfq-x86_64-w64-mingw32.exe
